709b8252a6ecaee6d667a5a23406d00631f36544
Stephen Brown <steve@evolvedlight.co.uk>
2015-10-29 18:49:53 +0100

    Initial commit

---
+++ src/uk/stephenbrown/Main.java
@@ -0,0 +1,16 @@
+package uk.stephenbrown;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int secondsToSleep = Integer.parseInt(args[0]);
+        System.out.println(String.format("I am sleeping for {0} seconds", secondsToSleep));
+        try {
+            Thread.sleep(1000*secondsToSleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Exiting");
+        }
+    }
+}
